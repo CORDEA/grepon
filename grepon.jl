7ee5c2ed529e11d316b12f09e5642387bfa0ad8c
Yoshihiro Tanaka <contact@cordea.jp>
2020-09-17 17:07:35 +0900

    group with created_at

--- grepon.jl
+++ grepon.jl
@@ -1,4 +1,5 @@
 import GitHub
+import Dates
 
 if length(ARGS) <= 2
     return
@@ -9,4 +10,18 @@ auth = GitHub.authenticate(ENV["GITHUB_TOKEN"])
 
 params = Dict("type" => "public", "sort" => "created")
 repos, next = GitHub.repos(owner; auth = auth, params = params)
-println(length(repos))
+
+range = Dict()
+date = floor(Dates.now() + Dates.Month(1), Dates.Month)
+for repo in repos
+    if date <= repo.created_at + Dates.Month(1)
+       global date -= Dates.Month(1)
+    end
+    if haskey(range, date)
+        push!(range[date], repo)
+    else
+        range[date] = [repo]
+    end
+end
+
+println(range)
